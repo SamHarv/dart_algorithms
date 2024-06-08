32c486dd8c2b92ae728a35606c079c580fbbb3c5
SamHarv <srharvey93@gmail.com>
2024-06-08 16:01:39 +1000

    Graph Added

--- bin/dart_algorithms.dart
+++ bin/dart_algorithms.dart
@@ -10,6 +10,7 @@
 // import 'package:dart_algorithms/data_structures/trees/heap.dart';
 // import 'package:dart_algorithms/search/binary_search.dart';
 
+import 'package:dart_algorithms/data_structures/graphs/graph.dart';
 import 'package:dart_algorithms/sorting/heap_sort.dart';
 import 'package:dart_algorithms/sorting/merge_sort.dart';
 import 'package:dart_algorithms/sorting/radix_sort.dart';
@@ -255,10 +256,44 @@ void main() {
   // print("Sorted: $list");
 
   // Quick Sort
-  final list = <num>[9, 4, 10, 3];
-  print("Original: $list");
-  quickSortDutchFlag(list, 0, list.length - 1);
-  print("Sorted: $list");
+  // final list = <num>[9, 4, 10, 3];
+  // print("Original: $list");
+  // quickSortDutchFlag(list, 0, list.length - 1);
+  // print("Sorted: $list");
+
+  // Graph
+  // AdjacencyList
+  // final graph = AdjacencyList<String>();
+  // AdjacencyMatrix
+  final graph = AdjacencyMatrix<String>();
+  final singapore = graph.createVertex('Singapore');
+  final tokyo = graph.createVertex('Tokyo');
+  final hongKong = graph.createVertex('Hong Kong');
+  final detroit = graph.createVertex('Detroit');
+  final sanFrancisco = graph.createVertex('San Francisco');
+  final washingtonDC = graph.createVertex('Washington DC');
+  final austinTexas = graph.createVertex('Austin Texas');
+  final seattle = graph.createVertex('Seattle');
+  graph.addEdge(singapore, hongKong, weight: 300);
+  graph.addEdge(singapore, tokyo, weight: 500);
+  graph.addEdge(hongKong, tokyo, weight: 250);
+  graph.addEdge(tokyo, detroit, weight: 450);
+  graph.addEdge(tokyo, washingtonDC, weight: 300);
+  graph.addEdge(hongKong, sanFrancisco, weight: 600);
+  graph.addEdge(detroit, austinTexas, weight: 50);
+  graph.addEdge(austinTexas, washingtonDC, weight: 292);
+  graph.addEdge(sanFrancisco, washingtonDC, weight: 337);
+  graph.addEdge(washingtonDC, seattle, weight: 277);
+  graph.addEdge(sanFrancisco, seattle, weight: 218);
+  graph.addEdge(austinTexas, sanFrancisco, weight: 297);
+  print(graph);
+  final cost = graph.weight(singapore, tokyo)?.toInt();
+  print('Cost: $cost');
+  print('San Francisco Ongoing Flights:');
+  for (final edge in graph.edges(sanFrancisco)) {
+    print(
+        'from: ${edge.source}, to: ${edge.destination}, cost: ${edge.weight}');
+  }
 }
 
 // Build Balanced Binary Search Tree
